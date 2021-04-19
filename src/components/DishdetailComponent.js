175043edd28bad04b1ade845f6553acc1931a5a8
Rohit <rrohitjee2018@gmail.com>
2021-04-19 11:23:31 +0530

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -27,7 +27,7 @@ class CommentForm extends Component{
 
   handleSubmit(values){
     this.toggleModal();
-    this.props.addComment(this.props.dishId,values.rating,values.author,values.comment);
+    this.props.postComment(this.props.dishId,values.rating,values.author,values.comment);
   }
 
   render(){
@@ -120,7 +120,7 @@ class CommentForm extends Component{
     }
 
 
-    function RenderDishComments({comments,addComment,dishId}){
+    function RenderDishComments({comments,postComment,dishId}){
      if(comments!=null)
      {
        //console.log(dish);
@@ -145,7 +145,7 @@ class CommentForm extends Component{
              <h4>Comments</h4>
              <div>
                {commt}
-               <CommentForm dishId={dishId} addComment={addComment}/>
+               <CommentForm dishId={dishId} postComment={postComment}/>
              </div>
            </div>
          )
@@ -206,7 +206,7 @@ class CommentForm extends Component{
           </div>
           <div className="col-12 col-md-5 m-1">
             <RenderDishComments comments={props.comments}
-            addComment={props.addComment} dishId={props.dish.id}/><br/>
+            postComment={props.postComment} dishId={props.dish.id}/><br/>
           </div>
         </div>
       </div>
--- src/redux/ActionCreaters.js
+++ src/redux/ActionCreaters.js
@@ -1,22 +1,57 @@
 import * as ActionTypes from './ActionTypes';
 import {baseUrl} from '../shared/baseUrl';
 
-export const addComment=(dishId,rating,author,comment)=>({
+export const addComment=(comment)=>({
   type:ActionTypes.ADD_COMMENT,
-  payload:{
+  payload:comment
+});
+
+export const postComment=(dishId,rating,author,comment)=>(dispatch)=>{
+
+  const newComment={
     dishId:dishId,
     author:author,
     rating:rating,
     comment:comment
 
   }
-});
+  newComment.date=new Date().toISOString();
+
+  return fetch(baseUrl+'comments',{
+    method:'POST',
+    body:JSON.stringify(newComment),
+    headers:{
+      'Content-Type':'application/json'
+    },
+    credentials:'same-origin'
+  })
+
+  .then(response => {
+    if (response.ok) {
+      return response;
+    } else {
+      var error = new Error('Error ' + response.status + ': ' + response.statusText);
+      error.response = response;
+      throw error;
+    }
+  },
+  error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+  })
+  .then(response => response.json())
+  .then(response =>dispatch(addComment(response)))
+  .catch(error =>  { console.log('post comments', error.message); alert('Your comment could not be posted\nError: '+error.message); });
+
+
+
+}
 
 //this is a thunk which return a function
 export const fetchDishes=()=>(dispatch)=>{
   dispatch(dishesLoading(true));
 
-    return fetch(baseUrl + 'dishees')
+    return fetch(baseUrl + 'dishes')
     .then(response => {
         if (response.ok) {
           return response;
